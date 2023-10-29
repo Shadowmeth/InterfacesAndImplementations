fd407868bb4cc49ab2d245bcbc056b7397556056
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2023-10-29 09:51:18 +0000

    fixed macros in arith.h

--- arith/arith.h
+++ arith/arith.h
@@ -1,8 +1,8 @@
 #ifndef __ARITH_ARITH_H
 #define __ARITH_ARITH_H
 
-#define DIV_TRUNCATES_TO_0(x, y) 	(((x) / (y)) < 0)
-#define DIFF_SIGN(x, y) 			(((x) < 0) != ((y) < 0))
+#define DIV_TRUNCATES_TO_0(x, y)	(((x) / (y)) < 0)
+#define DIFF_SIGN(x, y)				(((x) < 0) != ((y) < 0))
 
 extern int arith_max(int x, int y);
 extern int arith_min(int x, int y);
